d91ab32f77bb58ad297cb07b56e609c5825b38b8
siva <siva@1boxoffice.ae>
2023-10-16 11:37:05 +0530

    test check

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-Header('Access-Control-Allow-Origin: *'); //for allow any domain, insecure
+Header('Access-Control-Allow-Origin: *'); //for allow any domain, insecures
 Header('Access-Control-Allow-Headers: *'); //for allow any headers, insecure
 Header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE'); //method allowed
 /**
